1b35bc4c79f62f821aae9409a5a6db0250c18a6c
Rafael Gustavo <88495051+RafaelGustavo45@users.noreply.github.com>
2025-04-12 17:23:42 -0300

    Update ArvoreTAD.java
    deixei os metodos de impressão mais semelhantes a como é mostrado no exercicio

--- JavaArvoreBinaria-master/src/arvorebinaria/ArvoreTAD.java
+++ JavaArvoreBinaria-master/src/arvorebinaria/ArvoreTAD.java
@@ -65,7 +65,7 @@ public class ArvoreTAD {
             return;
         }
         this.imprimeEmOrdemRec(n.esq);
-        System.out.println(n.elem);
+        System.out.print(n.elem+" ");
         this.imprimeEmOrdemRec(n.dir);
     }//imprime em ordem crescente, em linhas separadas
 
@@ -75,7 +75,7 @@ public class ArvoreTAD {
         if(n == null){
             return;
         }
-        System.out.println(n.elem);
+        System.out.print(n.elem+" ");
         this.imprimeEmOrdemRec(n.esq);
         this.imprimeEmOrdemRec(n.dir);
     }//imprime em formato pré-ordem (raiz-esq-dir)
@@ -93,7 +93,7 @@ public class ArvoreTAD {
         if (node != null) {
             imprimePosOrdem(node.esq);
             imprimePosOrdem(node.dir);
-            System.out.println(node.elem);
+            System.out.print(node.elem+ " ");
         }
 
     }
